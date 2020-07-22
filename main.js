9eee00b6422d0485cf10810fa3e58480709f0ce0
elantsev <elantsev.pa@gmail.com>
2020-07-22 11:32:09 +0300

    preview fileList

--- main.js
+++ main.js
@@ -350,3 +350,76 @@ function previewImage () {
         document.getElementById("uploadPreview").src = oFREvent.target.result;
     };
 };
+
+
+// PreviewFiles******************************************************************************************************
+
+previewFiles('.evidence .file')
+previewFiles('.appeal .file')
+
+function previewFiles(selector) {
+    const input = document.querySelector(`${selector}  input`);
+    const preview = document.querySelector(`${selector} .input-wrapper__fileList`);
+
+    input.style.opacity = 0;
+
+    input.addEventListener('change', updateImageDisplay);
+
+    function updateImageDisplay () {
+        while (preview.firstChild) {
+            preview.removeChild(preview.firstChild);
+        }
+
+        const curFiles = input.files;
+        if (curFiles.length === 0) {
+            const para = document.createElement('p');
+            para.textContent = 'No files currently selected for upload';
+            preview.appendChild(para);
+        } else {
+            const list = document.createElement('ol');
+            preview.appendChild(list);
+
+            for (const file of curFiles) {
+                const listItem = document.createElement('li');
+                const para = document.createElement('p');
+                para.textContent = `${file.name}, ${returnFileSize(file.size)}.`;
+                listItem.appendChild(para);
+                if (isImage(file)) {
+                    const image = document.createElement('img');
+                    image.src = URL.createObjectURL(file);
+
+                    listItem.appendChild(image);
+                }
+
+                list.appendChild(listItem);
+            }
+        }
+    }
+
+    const fileTypes = [
+        "image/apng",
+        "image/bmp",
+        "image/gif",
+        "image/jpeg",
+        "image/pjpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/tiff",
+        "image/webp",
+        "image/x-icon"
+    ];
+
+    function isImage (file) {
+        return fileTypes.includes(file.type);
+    }
+
+    function returnFileSize (number) {
+        if (number < 1024) {
+            return number + 'bytes';
+        } else if (number >= 1024 && number < 1048576) {
+            return (number / 1024).toFixed(1) + 'KB';
+        } else if (number >= 1048576) {
+            return (number / 1048576).toFixed(1) + 'MB';
+        }
+    }
+}
